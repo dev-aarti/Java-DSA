bae5bed45dc47f2d267399927ebad6e9098f9103
dev-aarti <aarticahauhan20@gmail.com>
2025-09-20 21:12:09 +0530

    commit

---
+++ Conditionals/CalculatorIfElse.java
@@ -0,0 +1,9 @@
+package Conditionals;
+//Write a program to create a calculator that performs basic arithmetic operations (add, subtract, multiply and divide) using switch case and functions. The calculator should input two numbers and an operator from user.
+import java.util.*;
+public class SwitchStatement {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
