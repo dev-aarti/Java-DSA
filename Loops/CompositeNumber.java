8ddf2d8b90bf86dc4e5434d8b31698c52c10e79f
dev-aarti <aarticahauhan20@gmail.com>
2025-09-21 21:41:14 +0530

    commit

--- Loops/CompositeNumber.java
+++ Loops/CompositeNumber.java
@@ -1,4 +1,19 @@
 package Loops;
-
+//wap to check if a number is composite or not.
+import java.util.*;
 public class CompositeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0;  //0 means prime
+        for(int i = 2; i<=n-1; i++){
+            if(n%i==0){
+                System.out.println(n + " is a composite number");
+                x = 1; // 1 means composite
+                break;
+            }
+        }
+        if(n==1) System.out.println("Neither Prime nor Composite");
+        else if(x==0) System.out.println("Prime Number");
+    }
 }
--- Loops/ContinueKeyword.java
+++ Loops/ContinueKeyword.java
@@ -1,4 +1,23 @@
 package Loops;
 
 public class ContinueKeyword {
+    public static void main(String[] args) {
+        for(int i = 1; i<=100; i++){
+            if(i%2!=0){
+                System.out.println(i+"  ");
+            }
+        }
+//        50 iterations
+//        for(int i = 1; i<=100; i+=2){
+//                System.out.println(i+"  ");
+//
+//        }
+//    continue
+        System.out.println();
+        for(int i = 1; i<=100; i++) {
+            if (i % 2 == 0) continue;
+            System.out.println(i);
+        }
+
+    }
 }
