11051ade75ebb0bf810b4d9292b5c34abe6bf830
dev-aarti <aarticahauhan20@gmail.com>
2025-09-26 23:41:04 +0530

    commit

--- ArraysQues/MergeSortedArray.java
+++ ArraysQues/MergeSortedArray.java
@@ -1,4 +1,35 @@
 package ArraysQues;
 
 public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] arr ={2, 4, 5, 31};
+        int[] brr ={9,12,18,21};
+        int a = arr.length;
+        int b = brr.length;
+        int[] crr = new int[a+b];
+        int i=0, j=0, k;
+        for( k=0; k<a+b; k++){
+            if(i<a&&j<b){
+                if(arr[i]<=brr[j]){
+                    crr[k] = arr[i];
+                    i++;
+                }
+                else{
+                    crr[k] = brr[j];
+                    j++;
+                }
+            }
+            else if( i==a){
+                crr[k] = brr[j];
+                j++;
+            }
+            else{
+                crr[k] = arr[i];
+                i++;
+            }
+        }
+        for(int ele: crr){
+            System.out.print(ele+" ");
+        }
+    }
 }
