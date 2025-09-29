051b8d70e36f64562830b75a9390c3906543187e
dev-aarti <aarticahauhan20@gmail.com>
2025-09-29 23:29:46 +0530

    commit

--- Strings/printAllSubStrings.java
+++ Strings/printAllSubStrings.java
@@ -1,4 +1,13 @@
 package Strings;
-
+//input a string and print all the substring of that string.
 public class printAllSubStrings {
+    public static void main(String[] args) {
+        String s = "Aarti";
+        for(int i=0; i<s.length(); i++){
+            for(int j = i+1; j<=s.length(); j++){
+                System.out.print(s.substring(i, j)+"  ");
+            }
+            System.out.println();
+        }
+    }
 }
--- StringsAndStringBuilder/Question1.java
+++ StringsAndStringBuilder/Question1.java
@@ -1,4 +1,18 @@
 package StringsAndStringBuilder;
-
+//Input a string and Update all the even positions in the string to character 'a'. Consider 0-based indexing.
 public class Question1 {
+    public static void main(String[] args) {
+        String s = "Abhishek";
+        String str = "";
+        for(int i = 0; i<s.length(); i++){
+            if(i%2==0){
+                str+='a';
+            }
+            else{
+                str += s.charAt(i);
+            }
+        }
+        s = str;
+        System.out.println(s);
+    }
 }
