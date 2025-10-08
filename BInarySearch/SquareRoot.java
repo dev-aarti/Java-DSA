baaab449c509539edb255fe1ba40b19fa77fb230
dev-aarti <aarticahauhan20@gmail.com>
2025-10-08 22:03:21 +0530

    commit

--- BInarySearch/SquareRoot.java
+++ BInarySearch/SquareRoot.java
@@ -1,4 +1,18 @@
 package BInarySearch;
 
 public class SquareRoot {
+    public static void main(String[] args) {
+
+        long x=24;
+        long lo=0, hi=x;
+        while(lo<=hi){
+            long mid = lo+(hi-lo)/2;
+            if(mid*mid==x){
+                System.out.println(mid);
+                break;
+            }
+            else if(mid*mid>x) hi =mid-1;
+            else lo=mid+1;
+        }
+    }
 }
